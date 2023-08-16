0e983eadd03fa16e20ca83aac4f3c01de695a058
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-08-16 12:30:55 +0200

    feat: add mutation to createSong

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,18 +1,41 @@
 import React, { useState }  from "react";
+import { gql, useMutation } from '@apollo/client';
+
+const ADD_SONG = gql`
+    mutation AddSong($title: String) {
+        addSong(title: $title) {
+            id
+            title
+        } 
+    }
+`;
 
 const SongCreate = () => {
-
     const [title, setTitle] = useState('');
+    const [addSong, { data, loading, error }] = useMutation(ADD_SONG);
 
     const handleChange = (event) => {
         setTitle(event.target.value);
     };
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        addSong({ variables: { title } });
+        setTitle('');
+    };
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message} </p>;
+
+   
     return (
     <div>
         <h3>Create a New Song</h3>
-        <label>Song Title:</label>
+        <form onSubmit={handleSubmit}>
+            <label>Song Title:</label>
             <input type="text" value={title} onChange={handleChange} />
+            <button type="submit">Add Song</button>
+        </form>
     </div>
     );
 }

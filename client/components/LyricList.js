fa079509d3c67de2103476cf41a6283ccb638215
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-08-18 12:41:05 +0200

    feat: add like buttons to lyrics and counter

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,14 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { gql, useMutation } from "@apollo/client";
+
+const LIKE_LYRIC = gql`
+    mutation LikeLyric($id: ID) {
+        likeLyric(id: $id) {
+            id
+            likes
+        }
+    }
+`;
 
 const LyricList = ({lyrics}) => {
+    const [likeLyric] = useMutation(LIKE_LYRIC);
+
+    const onLike = (id) => {
+        console.log(id);
+        likeLyric({ variables: { id } });
+    };
+
     const renderLyrics = () => {
-        return lyrics.map(({id, content}) => 
+        return lyrics.map(({id, content, likes}) => 
             <li key={id} className="collection-item">
                 {content}
+                <div className="vote-box">
+                    <i 
+                        className="material-icons"
+                        onClick={() => {onLike(id)}}
+                    >
+                            thumb_up
+                    </i>
+                    {likes}
+                </div>
             </li>
         )
     }
+
     return (
         <ul className="collection">
             {renderLyrics()}
